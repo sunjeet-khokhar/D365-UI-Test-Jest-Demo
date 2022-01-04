f6e13739260bfe6e41e8f1e6d879de2bd1a7bc47
Sunjeet Khokhaer <sunjeet81@gmail.com>
2022-01-04 13:03:42 +1300

    added tests
    these also read creds from Git Hub secrets

---
+++ spec/demo-ui-test.spec.ts
@@ -0,0 +1,44 @@
+import { XrmUiTest } from "d365-ui-test";
+import * as fs from "fs";
+import * as playwright from "playwright";
+import * as path from "path";
+
+const xrmTest = new XrmUiTest();
+let browser: playwright.Browser = null;
+let context: playwright.BrowserContext = null;
+let page: playwright.Page = null;
+
+describe("Basic operations UCI", () => {
+    beforeAll(async() => {
+        jest.setTimeout(60000);
+
+        await xrmTest.launch("chromium", {
+            headless: false,
+            args: [
+                '--disable-setuid-sandbox',
+                '--disable-infobars',
+                '--start-fullscreen',
+                '--window-position=0,0',
+                '--window-size=1920,1080',
+                '--user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.198 Safari/537.36"'
+            ]
+        })
+        .then(([b, c, p]) => {
+            browser = b;
+            context = c;
+            page = p;
+        });
+    });
+
+    test("Start D365", async () => {
+        await xrmTest.open(process.env.CRM_URL, { userName: process.env.CRM_USER_NAME, password: process.env.CRM_PASSWORD, passwordFieldSelector: "#password_input", userNameFieldSelector: "#userName_input" });
+    });
+
+    test("Open new account form", async () => {
+        await xrmTest.Navigation.openCreateForm("account");
+    });
+
+    afterAll(() => {
+        return xrmTest.close();
+    });
+});
